85f45a80e7a40d6d559e4ea4dc2b27dcab2946c6
eulucasilva <lucassilvasistemas@gmail.com>
2017-04-18 17:45:07 -0300

    padronização

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -80,7 +80,7 @@
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                            <h4 class="modal-title" id="myModalLabel"><strong>Dados do papel: {{$role->display_name}}</strong></h4>
+                                            <h4 class="modal-title" id="myModalLabel"><strong>Papel: {{$role->display_name}}</strong></h4>
                                         </div>
                                         <div class="modal-body">
                                             <div class="row">
